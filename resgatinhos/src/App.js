49ce417591e260ae2682cf2381e8ebc07854a4ea
MaisaPereiraMarcal <183663267+Migue-ux@users.noreply.github.com>
2025-10-08 15:59:49 -0300

    08/10/25
    08/10/25 - miguel

--- resgatinhos/src/App.js
+++ resgatinhos/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Adotar from "./Adotar/page-adotar";
+import Contato from "./Contato/page-contato";
+import QueroAdotar from "./Quero-adotar/page-quero-adotar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/contato" element={<Contato />} />
+          <Route path="/adotar" element={<Adotar />} />
+          <Route path="/quero-adotar" element={<QueroAdotar />} />
+
+      </Routes>
+    </Router>
   );
 }
-
-export default App;

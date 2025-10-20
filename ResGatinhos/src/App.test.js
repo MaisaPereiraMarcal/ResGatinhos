c8fab9c234bc792c947ef484a1bd8e8c2781bd26
camila <camilamedeirosdasilva239@gmail.com>
2025-10-20 15:57:23 -0300

    20/10
    20/10

--- ResGatinhos/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders main app content', () => {
-  render(<App />);
-  // Replace 'Main App Content' with actual text from your App component
-  const mainContent = screen.getByText(/Main App Content/i);
-  expect(mainContent).toBeInTheDocument();
-});
